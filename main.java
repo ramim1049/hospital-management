29425ebe52bc8131689bd3afa1fdca12a5b54afe
ramim1049 <63585201+ramim1049@users.noreply.github.com>
2020-04-13 15:44:16 +0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+
+package hospital.management.system;
+
+public class main {
+    public static void main(String[] args){
+        person p1 = new person("Ramin","01617296379");
+         p1.whoyouare()
+    }
+    
+}
